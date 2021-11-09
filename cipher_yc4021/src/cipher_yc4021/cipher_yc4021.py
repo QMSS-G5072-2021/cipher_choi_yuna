b646f09f53814cedaf1f59fe3fc3e6f4fa70f932
yc4021 <91342199+yc4021@users.noreply.github.com>
2021-11-08 22:45:17 -0500

    Update cipher_yc4021.py

---
+++ cipher_yc4021/src/cipher_yc4021/cipher_yc4021.py
@@ -0,0 +1,37 @@
+def cipher(text, shift, encrypt=True):
+    """
+    Concatenates two pandas categoricals.
+
+    Parameters
+    ----------
+    text : str
+      The text that will be encrypted/decrypted.
+    shift : int
+      The number of positions you move right or lift. 
+     encrypt : bool
+      A boolean that defines whether you are encrypting or decrypting text. 
+
+    Returns
+    -------
+    str
+      The ending result will be a string. 
+
+    Examples
+    --------
+    >>> from cipher_yc4021 import cipher_yc4021
+    >>> text = 'Halloween'
+    >>> shift = 5
+    >>> cipher(text, shift, encrypt = True)
+    'MfqqtBjjs'
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
