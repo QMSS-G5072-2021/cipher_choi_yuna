bbbede451739611354859b162873ca8ec5250fee
yc4021 <91342199+yc4021@users.noreply.github.com>
2021-11-08 23:42:51 -0500

    Update test_cipher_yc4021.py

--- cipher_yc4021/tests/test_cipher_yc4021.py
+++ cipher_yc4021/tests/test_cipher_yc4021.py
@@ -1,1 +1,7 @@
 from cipher_yc4021 import cipher_yc4021
+    
+def test_cipher_word():
+    example = 'Halloween'
+    expected = 'MfqqtBjjs'
+    actual = cipher(example, 5)
+    assert actual == expected
