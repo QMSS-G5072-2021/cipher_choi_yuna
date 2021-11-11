69d3b21e34429edb49b86f71c586d2822c675924
yc4021 <91342199+yc4021@users.noreply.github.com>
2021-11-10 21:23:29 -0500

    Update test_cipher_yc4021.py

--- cipher_yc4021/tests/test_cipher_yc4021.py
+++ cipher_yc4021/tests/test_cipher_yc4021.py
@@ -1,4 +1,17 @@
 from cipher_yc4021 import cipher_yc4021
+import pytest
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
     
 def test_cipher_word():
     example = 'Halloween'
